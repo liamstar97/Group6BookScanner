ae450b785016e8cd0793610e78133df2a04bda23
liamstar97 <liamstar@gmail.com>
2017-11-01 14:38:41 -0700

    changing all method types from public to private.

--- src/BookScanner.java
+++ src/BookScanner.java
@@ -15,35 +15,31 @@ public class BookScanner {
         println("File URL: " + fileLocation);
     }
 
-    public static String getWorkName(Scanner input) {
+    private static String getWorkName(Scanner input) {
         print("Please enter the works name: ");
         return input.nextLine();
     }
 
-    public static String getAuthorName(Scanner input){
+    private static String getAuthorName(Scanner input){
         print("Please enter the author name: ");
         return input.nextLine();
     }
 
-    public static String getFileLocation(Scanner input) {
+    private static String getFileLocation(Scanner input) {
         print("Please enter the file location (full path): ");
         return input.nextLine();
     }
 
-    public static String getLocalFileName(String fileLocation) {
+    private static String getLocalFileName(String fileLocation) {
         String[] fileLocationArray = fileLocation.split("\\\\");
         return fileLocationArray[fileLocationArray.length - 1];
     }
 
-    public static void println(String s) {
+    private static void println(String s) {
         System.out.println(s);
     }
 
-    public static void print(String s) {
+    private static void print(String s) {
         System.out.print(s);
-    }
-
-    public static void method() {
-
     }
 }
